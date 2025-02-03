e92f87f96d634dbd844574e207c5fd15bd324780
nigelmashayamombe <mashayamombenigel424@gmail.com>
2025-02-03 19:18:54 +0200

    added the vercel insights package to

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { CustomCursor } from '@/components/custom-cursor';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
